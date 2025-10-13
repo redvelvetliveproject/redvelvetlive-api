21d21656ae6cfff16e8965168ddf9aa6328bcebb
redvelvetliveproject <redvelvet.live.project@gmail.com>
2025-10-13 02:21:28 -0500

    Crear index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,14 @@
+// backend/src/models/index.js
+export { default as Model } from "./Model.js";
+export { default as User } from "./User.js";
+export { default as Payment } from "./Payment.js";
+export { default as PaymentOrder } from "./PaymentOrder.js";
+export { default as Wallet } from "./Wallet.js";
+export { default as WalletChallenge } from "./WalletChallenge.js";
+export { default as Tip } from "./Tip.js";
+export { default as Stream } from "./Stream.js";
+export { default as Subscription } from "./Subscription.js";
+export { default as Ticket } from "./Ticket.js";
+export { default as Notification } from "./Notification.js";
+export { default as Post } from "./Post.js";
+export { default as RefreshToken } from "./RefreshToken.js";
